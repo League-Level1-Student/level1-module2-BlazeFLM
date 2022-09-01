1480f98313e1eef9be8ae23dc811721b3fda3e01
student <student@ip-172-31-62-34.us-west-2.compute.internal>
2022-09-01 15:28:42 -0700

    3:27 PM 9/1/22 IN SCHOOL

--- src/_04_backpack/BackpackRunner.java
+++ src/_04_backpack/BackpackRunner.java
@@ -2,6 +2,15 @@ package _04_backpack;
 
 public class BackpackRunner {
 	public static void main(String[] args) {
+		Pencil pencil = new Pencil();
+		Ruler ruler = new Ruler();
+		Textbook book = new Textbook();
 		new Backpack().packAndCheck();
+	Backpack backpack = new Backpack();
+	backpack.putInBackpack(pencil);
+	backpack.putInBackpack(ruler);
+	backpack.putInBackpack(book);
+	log();
+	goToSchool();
 	}
 }
