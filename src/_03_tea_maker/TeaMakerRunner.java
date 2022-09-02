34ae2e6008c937c16493c0c1e4b3507147a43e0c
student <student@ip-172-31-58-133.us-west-2.compute.internal>
2022-09-01 17:12:09 -0700

    5:11 PM 9/1/22 NOT IN CLASS

--- src/_03_tea_maker/TeaMakerRunner.java
+++ src/_03_tea_maker/TeaMakerRunner.java
@@ -2,6 +2,12 @@ package _03_tea_maker;
 
 public class TeaMakerRunner {
 	public static void main(String[] args) {
+		TeaBag teabag = new TeaBag("GREEN");
 		
+		Kettle kettle = new Kettle();
+		kettle.getWater();
+		kettle.boil();
+		Cup cup = new Cup();
+		cup.makeTea(teabag, kettle.getWater());
 	}
 }
--- src/_04_backpack/BackpackRunner.java
+++ src/_04_backpack/BackpackRunner.java
@@ -5,12 +5,11 @@ public class BackpackRunner {
 		Pencil pencil = new Pencil();
 		Ruler ruler = new Ruler();
 		Textbook book = new Textbook();
-		new Backpack().packAndCheck();
-	Backpack backpack = new Backpack();
-	backpack.putInBackpack(pencil);
-	backpack.putInBackpack(ruler);
-	backpack.putInBackpack(book);
-	log();
-	goToSchool();
+		Backpack backpack = new Backpack();
+		backpack.putInBackpack(pencil);
+		backpack.putInBackpack(ruler);
+		backpack.putInBackpack(book);
+		backpack.packAndCheck();
+
 	}
 }
